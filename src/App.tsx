d05f68781ab278388794e9cf2d129aae2e2ee8e3
mbagatini <morganabagatini@gmail.com>
2021-11-05 13:56:29 -0300

    React modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isTransactionModalOpened, setIsTransactionModalOpened] =
+    useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsTransactionModalOpened(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsTransactionModalOpened(false);
+  }
+
+  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+  Modal.setAppElement("#root");
+
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+
+      <Modal
+        isOpen={isTransactionModalOpened}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import logo from "../../assets/logo.svg";
 
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenTransactionModal: () => void;
+}
+
+export function Header({ onOpenTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt="Money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
