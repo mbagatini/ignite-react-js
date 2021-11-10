863a820fab066793746847be2c07d3be8c0ab618
mbagatini <morganabagatini@gmail.com>
2021-11-10 13:35:57 -0300

    Organização projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>ig news</title>
+      </Head>
+      <h1>fala cupinxa</h1>
+    </>
+  );
+};
+
+export default Home;
