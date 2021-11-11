2e1feb80fa099217ff23ed305ee10950c371a382
mbagatini <morganabagatini@gmail.com>
2021-11-11 09:45:44 -0300

    SSR: buscar preço da assinatura do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
+import { stripe } from "../services/stripe";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./index.module.scss";
 
-const Home: NextPage = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,16 +27,34 @@ const Home: NextPage = () => {
           </h1>
           <p>
             Get acess to all the publications{" "}
-            <span className={styles.attention}>for $9.90 month</span>
+            <span className={styles.attention}>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
-};
+}
 
-export default Home;
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Busca o produto da API
+  const price = await stripe.prices.retrieve("price_1JublPDJkjQsgFuZ2SYNF1nF");
+
+  // Dados do produto
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format((price.unit_amount || 0) / 100), // Valor em centavos
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
