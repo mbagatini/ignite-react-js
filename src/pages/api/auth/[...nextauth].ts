0cc2375c54ee583abaa05adcab2952dc9b8f9d4b
mbagatini <morganabagatini@gmail.com>
2021-11-12 15:20:37 -0300

    Integrando a autentiação com o Fauna DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+import { query as q } from "faunadb";
+import { fauna as faunaClient } from "../../../services/fauna";
+
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
@@ -11,4 +14,44 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+
+  // Callbacks are asynchronous functions you can use to control what happens when an action is performed.
+  callbacks: {
+    async signIn({ name, email }) {
+      if (!email) {
+        console.error("User email not found");
+        return false;
+      }
+
+      // Insere no Fauna os dados do usuário autenticado se o mesmo não existir. Sintaxe FQL
+      try {
+        await faunaClient
+          .query(
+            q.If(
+              q.Not(
+                q.Exists(q.Match(q.Index("user_email"), q.Casefold(email)))
+              ),
+              q.Create(q.Collection("users"), {
+                data: { name, email },
+              }),
+              q.Update(
+                q.Select(
+                  ["ref"],
+                  q.Get(q.Match(q.Index("user_email"), q.Casefold(email)))
+                ),
+                {
+                  data: { name },
+                }
+              )
+            )
+          )
+          .then((ret) => console.log(ret));
+      } catch (error) {
+        console.error("Error AUTH: \n" + error);
+        return false;
+      }
+
+      return true;
+    },
+  },
 });
