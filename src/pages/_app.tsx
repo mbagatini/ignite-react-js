4946e76d3e0ec59888483ce7e9fa4e7239b42112
mbagatini <morganabagatini@gmail.com>
2021-11-12 11:57:24 -0300

    Next auth: atentitação com o github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as NextSessionProvider } from "next-auth/client";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextSessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextSessionProvider>
   );
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+      scope: "read:user",
+    }),
+  ],
+});
