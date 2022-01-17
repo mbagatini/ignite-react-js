486816e9a067d967f9a0df8c8a1b809c164d2b36
mbagatini <morganabagatini@gmail.com>
2022-01-17 17:20:15 -0300

    Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex, VStack } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <VStack>
+      <Header />
+
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </VStack>
+  );
 }
