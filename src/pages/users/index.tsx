7f2aa5c58bf69b95a6f24438eee63b26d1bd0ffb
mbagatini <morganabagatini@gmail.com>
2022-01-18 15:16:18 -0300

    Paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -70,6 +71,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
