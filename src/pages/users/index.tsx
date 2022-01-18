c7b5d9f8ca8e42475d1e09d42c200d3bb6271155
mbagatini <morganabagatini@gmail.com>
2022-01-18 15:06:20 -0300

    Listagem de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,77 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>User</Th>
+                <Th>Registered since</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Morgana Bagatini</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      morganabagatini@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>20 Nov 2021</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
