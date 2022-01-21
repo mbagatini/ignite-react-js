ce39487b5fed90eea2b574806bce5766e85d05ee
mbagatini <morganabagatini@gmail.com>
2022-01-21 19:00:25 -0300

    Trabalhando com referencia no componente input

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,13 +4,21 @@ import {
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { ForwardRefRenderFunction, forwardRef } from "react";
 
+/**
+ * Não é possível passar a ref de um elemento através das props do componente
+ * para isso se usa o forwardRef que recebe o elemento por parâmetro
+ */
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, ...rest }: InputProps,
+  ref
+) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -22,8 +30,11 @@ export function Input({ name, label, ...rest }: InputProps) {
         variant="filled" // Tipo de input
         size="lg"
         _hover={{ bgColor: "gray.900" }} // Estilo de estados
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
