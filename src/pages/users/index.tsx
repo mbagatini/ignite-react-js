9b86fd2e204d1ef28c64eb107b2ebf7d1824de7d
mbagatini <morganabagatini@gmail.com>
2022-02-01 16:28:39 -0300

    Listando os usuários da API

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,14 +15,28 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+interface User {
+  name: string;
+  email: string;
+  createdAt: Date;
+}
+
 export default function UserList() {
   const isLarge = useBreakpointValue({ base: false, lg: true });
+  const [users, setUsers] = useState([] as User[]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((result) => setUsers(result.users));
+  }, []);
 
   return (
     <Box>
@@ -57,24 +71,34 @@ export default function UserList() {
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>User</Th>
-                {isLarge && <Th>Registered since</Th>}
+                {isLarge && <Th>Created at</Th>}
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Morgana Bagatini</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      morganabagatini@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isLarge && <Td>20 Nov 2021</Td>}
-              </Tr>
+              {users.map((user, idx) => {
+                return (
+                  <Tr key={idx}>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">{user.name}</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          {user.email}
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isLarge && (
+                      <Td>
+                        {new Intl.DateTimeFormat("pt-BR", {
+                          dateStyle: "medium",
+                        }).format(new Date(user.createdAt))}
+                      </Td>
+                    )}
+                  </Tr>
+                );
+              })}
             </Tbody>
           </Table>
 
