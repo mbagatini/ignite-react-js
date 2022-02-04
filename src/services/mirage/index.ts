50695d387515ab83ca5a06ef0bedb7e30c1eb61d
mbagatini <morganabagatini@gmail.com>
2022-02-04 14:05:17 -0300

    MirageJS: paginação dos usuarios

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import { faker } from "@faker-js/faker";
 
 type User = {
@@ -29,7 +29,24 @@ export function initServer() {
       this.namespace = "api";
       this.timing = 750; // delay das consultas
 
-      this.get("/users");
+      this.get("/users", function (this: any, schema, request) {
+        const { page = 1, perPage = 10 } = request.queryParams;
+
+        // Serializa os dados para o Miraje enteder o tipo de dado
+        const users = this.serialize(schema.all("user")).users;
+
+        const pageStart = (Number(page) - 1) * Number(perPage);
+        const pageEnd = Number(page) * Number(perPage);
+
+        const paginatedUsers = users.slice(pageStart, pageEnd);
+
+        return new Response(
+          200,
+          { "x-total-count": String(users.length) },
+          { users: paginatedUsers }
+        );
+      });
+
       this.post("/users");
 
       // reseta as rotas padrão para não conflitar com o Next
