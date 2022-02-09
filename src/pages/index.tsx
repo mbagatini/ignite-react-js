430b166ad4344cd0743f37f14f3400f72a2608c2
mbagatini <morganabagatini@gmail.com>
2022-02-09 11:00:18 -0300

    Criando projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    console.log(email, password);
+  }
+
+  return (
+    <form className={styles.container}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign in</button>
+    </form>
+  );
+}
