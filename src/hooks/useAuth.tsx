639ffdd9de8c932d0fe96f60bb975eb272f281bb
mbagatini <morganabagatini@gmail.com>
2022-02-10 11:09:11 -0300

    SignIn functional salvando os dados do JWT

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Router from "next/router";
+import { setCookie, parseCookies } from "nookies";
 import { api } from "../services/api";
 
 interface User {
@@ -30,16 +32,53 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
   const isAuthenticated = !!user;
 
+  /**
+   * Carrega os dados do usuário somente quando ele realizar o primeiro acesso
+   */
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", { email, password });
-      const { permissions, roles } = response.data;
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      // Salva os dados do usuário em um cookie
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 30 * 24 * 60 * 60, // 30 dias
+        path: "/", // Caminhos com acesso ao cookie
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 30 * 24 * 60 * 60, // 30 dias
+        path: "/", // Caminhos com acesso ao cookie
+      });
 
       setUser({
         email,
         permissions,
         roles,
       });
+
+      /**
+       * Atualiza os headers da api para salvar o token
+       */
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
     } catch (error) {}
   }
 
