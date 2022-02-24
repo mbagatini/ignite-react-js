cfab2b1fd3d352871a16179b3e3355a9323c1300
Morgana <morganabagatini@gmail.com>
2022-02-23 21:19:50 -0300

    Validação de permissões por SSR

--- src/hooks/useCan.ts
+++ src/hooks/useCan.ts
@@ -1,3 +1,4 @@
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 import { useAuth } from "./useAuth";
 
 interface useCanProps {
@@ -12,30 +13,5 @@ export function useCan({ permissions, roles }: useCanProps) {
     return false;
   }
 
-  /**
-   * Permissões
-   */
-  if (user.permissions && permissions?.length) {
-    const hasAllPermissions = permissions.every((permission) => {
-      return user.permissions.includes(permission);
-    });
-    if (!hasAllPermissions) {
-      return false;
-    }
-  }
-
-  /**
-   * Papéis
-   */
-  if (user.roles && roles?.length) {
-    const hasAllRoles = roles.some((role) => {
-      return user.roles.includes(role);
-    });
-
-    if (!hasAllRoles) {
-      return false;
-    }
-  }
-
-  return true;
+  return validateUserPermissions({ user, permissions, roles });
 }
--- src/utils/checkSSRAuth.ts
+++ src/utils/checkSSRAuth.ts
@@ -4,19 +4,52 @@ import {
   GetServerSidePropsResult,
 } from "next";
 import { parseCookies } from "nookies";
+import decode from "jwt-decode";
 
 import { clearCookies } from "../hooks/useAuth";
 import { AuthError } from "../services/errors/AuthError";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type checkSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
 
 /**
  * o T é a tipagem da função e é por causa dele que o typescript
  * consegue sugerir as propriedades do retorno
  */
-export function checkSSRAuth<T>(callback: GetServerSideProps<T>) {
+export function checkSSRAuth<T>(
+  callback: GetServerSideProps<T>,
+  options?: checkSSRAuthOptions
+) {
   return async (
     context: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<T> | undefined> => {
     const cookies = parseCookies(context);
+    const token = cookies["nextauth.token"];
+
+    // Token decodificado
+    const user = decode<{ permissions: string[]; roles: string[] }>(token);
+
+    if (options) {
+      const { permissions, roles } = options;
+
+      const userCanAccess = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userCanAccess) {
+        return {
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
+    }
 
     /**
      * Redireciona o usuário para o login se ele não estiver autenticado
