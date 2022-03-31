14d19d6abf0060d6894c14b1f98f630164b55033
Morgana <morganabagatini@gmail.com>
2022-03-30 22:42:45 -0300

    Listagem dos cards

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,38 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function hadleViewImage(url: string): void {
+    setSelectedImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards?.map(card => {
+          return (
+            <Card
+              data={card}
+              key={card.id}
+              viewImage={() => hadleViewImage(card.url)}
+            />
+          );
+        })}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        imgUrl={selectedImageUrl}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </>
   );
 }
